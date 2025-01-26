dc58c9285488a5f16490e6a4454c778f88df2583
3p1cPh4r40h <russaz09@hotmail.com>
2025-01-26 14:50:10 -0500

    Added commented example agent
    Added a commented example agent to the top of the "ExampleAgentSystem.py" script that better shows how we will be using SPADE in our project.

--- ExampleAgentSystem.py
+++ ExampleAgentSystem.py
@@ -1,11 +1,53 @@
-# Before you run this program ensure that you have SPADE installed with "pip install spade". 
-# In a seperate terminal run the commmand "spade run" to start the SPADE server.
+# Before you run this program ensure that you have SPADE installed with "pip install spade".
+# In a separate terminal run the command "spade run" in the project folder to start the SPADE server.
 
-import asyncio
 import random
+import asyncio
+
+# These are the libraries that you will generally need to import for your agent
+# (this does not count any AI or data processing libraries you use)
+import json
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
 from spade.message import Message
+
+# Here is an example agent, it extends the "Agent" class we imported from SPADE.
+# **Note: This agent is NOT run when you run the example script.
+class ExampleAgent(Agent):
+    # Most agents will need to await signals from other agents, for this we will use
+    # the CyclicBehaviour because it will handle loop behaviour and asynchronous messages.
+    class AwaitingBehaviour(CyclicBehaviour):
+        # We set up an async run function which will be run periodically in the main
+        async def run(self):
+            # Wait for message
+            msg = await self.receive(timeout=5) # Check for messages in 5 second interval
+            # If we receive a message...
+            if msg:
+                # Create a try catch block to check for invalid JSON messages
+                try:
+                    # Unpack the information in the message (stored in JSON format)
+                    data = json.loads(msg.body)
+                    # Data is now a dictionary with your desired variable names as keys with their corresponding values
+                    example_value = data["example_value"]
+                    # Now you can do logic (i.e. run your model) on the inputs
+                    model_result = example_value/2
+                    # Create a Message object to store your response
+                    response = Message(to="facilitating@localhost")
+                    # Store the response in a json object
+                    response.body = json.dumps({
+                        "key":"value",
+                        "key2":"value",
+                        "model_result":model_result
+                    })
+
+                    # Send the message
+                    await self.send(response)
+                # If we encounter an error we can specify which agent caught the error, and the message in question
+                except json.JSONDecodeError:
+                    print("[ExampleAgent] Invalid message format: {msg.body}")
+
+
+
 
 class InformationGatheringAgent(Agent):
     class GatherInformationBehaviour(CyclicBehaviour):
